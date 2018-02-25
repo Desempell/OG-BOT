211a1ae6ca69ee0a9694dd96f7ce4a75e0ed4aaf
Desempell <36824319+Desempell@users.noreply.github.com>
2018-02-25 22:49:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const Discord = require(`discord.js`);
+const client = new Discord.Client();
+
+client.login("NDE1MTM4MzE3MzgyOTc1NTAw.DWxjQQ.nxBUS90dWTE6qIfF7yTyUvTsL9g");
+
+client.on("message",(message)=>{
+if(message.content=="og!Dark")
+    message.reply("О,вы тоже знаете моего создателя?")});
+
+    client.on("message",(message)=>{
+        if(message.content=="og!Egorka"){
+            message.reply("Это тот чсвшный модератор? :thinking:")};
+      });     
+
+    client.on('message', message => {
+        if (message.content == "og!Avatar") {
+            message.reply(message.author.avatarURL);
+        }
+    });
+
+    client.on("message",message => {
+        if (message.content == "og!info") {
+            message.reply(message.channel.send({embed: {
+                color: 6667003,
+                author: {
+                  name: client.user.username,
+                  icon_url: client.user.avatarURL
+                },
+                title: "Орден Геймеров",
+                description: "Это сервер для истинных геймеров!",
+                fields: [{
+                    name: "Информация",
+                    value: "Вы можете прочитать правила в канале #rules и посмотреть систему ролей в #welcome"
+                  },
+         ]
+            }}))
+        }});
+    
+    client.on("message",message => {
+        if (message.content == "og!help") {
+       message.reply(message.channel.send({embed: {
+        color: 5367703,
+        author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL
+        },
+        title: "Это список команд",
+        description: "Этот список не конечный,новые команды скоро будут.",
+        fields: [{
+            name: "og!Dark",
+            value: "- Вопрос от бота"
+          },
+          {
+            name: "og!Egorka",
+            value: "- Чудо вопрос"
+          },
+          {
+            name: "og!info",
+            value: "- Информация о нашем сервере"
+          },
+         {
+            name: "og!Avatar",
+            value: "- Бот скинет вашу аватарку"
+        },
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: client.user.avatarURL,
+          text: "© Dark"
+        }
+      }
+    })
+)}});      
+    
