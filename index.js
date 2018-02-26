e03ec6c3ab2e3d4174371c7170a11e4214c3417f
Desempell <36824319+Desempell@users.noreply.github.com>
2018-02-26 18:55:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 const Discord = require(`discord.js`);
 const client = new Discord.Client();
-
-client.login("");
-
-if(message.author.bot) return;
-if(message.content.indexOf(process.env.PREFIX) !== 0) return;
-const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
-
-
-client.on("message",(message)=>{
-if(message.content=="og!Dark")
-    message.reply("О,вы тоже знаете моего создателя?")});
-
-    client.on("message",(message)=>{
-        if(message.content=="og!Egorka"){
-            message.reply("Это тот чсвшный модератор? :thinking:")};
-      });     
-
-    client.on('message', message => {
-        if (message.content == "og!Avatar") {
-            message.reply(message.author.avatarURL);
-        }
-    });
-
-    client.on("message",message => {
-        if (message.content == "og!info") {
-            message.reply(message.channel.send({embed: {
-                color: 6667003,
-                author: {
-                  name: client.user.username,
-                  icon_url: client.user.avatarURL
-                },
-                title: "Орден Геймеров",
-                description: "Это сервер для истинных геймеров!",
-                fields: [{
-                    name: "Информация",
-                    value: "Вы можете прочитать правила в канале #rules и посмотреть систему ролей в #welcome"
-                  },
-         ]
-            }}))
+const prefix = "og!";
+ 
+client.login("NDE1MTM4MzE3MzgyOTc1NTAw.DXWwJg.Nfw6PdyujhhCnvfC6Vj_QDBVZOA").catch(err => {console.log(err);});
+ 
+ 
+client.on("message", async (message) =>{
+ 
+    if(message.author.bot) return;
+    if(message.content.indexOf(prefix) !== 0) return;
+ 
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === "Dark")
+        message.reply("О,вы тоже знаете моего создателя?");
+ 
+    if(command === "Egorka"){
+        message.reply("Это тот чсвшный модератор? :thinking:")};
+ 
+    if (command === "avatar") {
+        let member = message.mentions.members.first();
+        if (!member)
+            return message.channel.send('Бип-Буп-Бип');
+        message.reply(member.user.avatarURL);
+    }
+ 
+    if (command === "info") {
+        message.reply({embed: {
+            color: 6667003,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            title: "Орден Геймеров",
+            description: "Это сервер для истинных геймеров!",
+            fields: [{
+                name: "Информация",
+                value: "Вы можете прочитать правила в канале #rules и посмотреть систему ролей в #welcome"
+              },
+             ]
         }});
-    
-    client.on("message",message => {
-        if (message.content == "og!help") {
-       message.reply(message.channel.send({embed: {
+    }
+   
+    if (command === "help")
+    message.reply({embed: {
         color: 5367703,
         author: {
           name: client.user.username,
@@ -53,20 +53,20 @@ if(message.content=="og!Dark")
         title: "Это список команд",
         description: "Этот список не конечный,новые команды скоро будут.",
         fields: [{
-            name: "og!Dark",
+            name: prefix+"Dark",
             value: "- Вопрос от бота"
           },
           {
-            name: "og!Egorka",
+            name: prefix+"Egorka",
             value: "- Чудо вопрос"
           },
           {
-            name: "og!info",
+            name: prefix+"info",
             value: "- Информация о нашем сервере"
           },
          {
-            name: "og!Avatar",
-            value: "- Бот скинет вашу аватарку"
+            name: prefix+"Avatar",
+            value: "- Бот скинет аватарку того кого вы упомяните"
         },
         ],
         timestamp: new Date(),
@@ -74,6 +74,6 @@ if(message.content=="og!Dark")
           icon_url: client.user.avatarURL,
           text: "© Dark"
         }
-      }
-    })
-)}});      
+   
+  }})
+});
