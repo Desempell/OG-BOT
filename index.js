9c71cf5f3f8ac944322803e10df616c3230a2dcf
Desempell <36824319+Desempell@users.noreply.github.com>
2019-03-01 16:13:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = 'd!'
 
-client.login(process.env.BOT_TOKEN);
+ client.login(process.env.BOT_TOKEN).catch(err => {console.log(err)});
 console.log(`Всё ок`);
 
 client.on("ready", () => {
