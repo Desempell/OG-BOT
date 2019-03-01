a07eb60af4c1d56e545db1736c86c4fb89b49c1c
Desempell <36824319+Desempell@users.noreply.github.com>
2019-03-01 15:51:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,61 @@
-const Discord = require(`discord.js`);
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "og!";
- 
-client.login("").catch(err => {console.log(err);});
- 
- 
-client.on("message", async (message) =>{
- 
-    if(message.author.bot) return;
-    if(message.content.indexOf(prefix) !== 0) return;
- 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
- 
-    if(command === "Dark")
-        message.reply("О,вы тоже знаете моего создателя?");
- 
-    if(command === "Egorka"){
-        message.reply("Это тот чсвшный модератор? :thinking:")};
- 
-    if (command === "avatar") {
-        let member = message.mentions.members.first();
-        if (!member)
-            return message.channel.send('Бип-Буп-Бип');
-        message.reply(member.user.avatarURL);
-    }
- 
-    if (command === "info") {
-        message.reply({embed: {
-            color: 6667003,
-            author: {
-              name: client.user.username,
-              icon_url: client.user.avatarURL
-            },
-            title: "Орден Геймеров",
-            description: "Это сервер для истинных геймеров!",
-            fields: [{
-                name: "Информация",
-                value: "Вы можете прочитать правила в канале #rules и посмотреть систему ролей в #welcome"
-              },
-             ]
-        }});
-    }
-   
-    if (command === "help")
-    message.reply({embed: {
-        color: 5367703,
-        author: {
-          name: client.user.username,
-          icon_url: client.user.avatarURL
-        },
-        title: "Это список команд",
-        description: "Этот список не конечный,новые команды скоро будут.",
-        fields: [{
-            name: prefix+"Dark",
-            value: "- Вопрос от бота"
-          },
-          {
-            name: prefix+"Egorka",
-            value: "- Чудо вопрос"
-          },
-          {
-            name: prefix+"info",
-            value: "- Информация о нашем сервере"
-          },
-         {
-            name: prefix+"Avatar",
-            value: "- Бот скинет аватарку того кого вы упомяните"
-        },
-        ],
-        timestamp: new Date(),
-        footer: {
-          icon_url: client.user.avatarURL,
-          text: "© Dark"
-        }
-   
-  }})
+const prefix = 'd!'
+
+client.login("");
+console.log(`Всё ок`);
+
+client.on("ready", () => {
+    client.channels.get("529981802497179648").send(`Бот включился. На сервере ${client.users.size} участника. Всего серверов со мной ${client.guilds.size}. Количество каналов равно ${client.channels.size}-ти.`)
+client.user.setActivity(`Codding`);
 });
+
+client.on("message", (message) => { 
+    if(message.content === prefix + "тест") {
+    const embed = new Discord.RichEmbed()
+
+    .setTitle('Ня')
+    .setAuthor(`name`)
+    .setColor(0xFFFFFF)
+    .setDescription(`Кекc`)
+    .setImage(`https://cdn.discordapp.com/avatars/322699868865036290/a_2b6e99c84706c7afb3963e4827a3dbc7.gif`)
+    
+        message.channel.send(embed)
+      } 
+     
+});
+
+client.on("message", (message) => {
+    if(message.content === prefix + "аватар") {
+        message.reply(message.author.avatarURL);
+    }
+});
+let a = ["Да", "Нет", "Возможно","Я лучше промолчу","Маловероятно", "Спроси у хохла"];
+client.on("message", (message) => {
+    const embed = new Discord.RichEmbed();
+    embed.setTimestamp();
+    embed.setTitle(`Ответ`);
+    embed.setColor(0xffd9d9);
+    embed.setDescription(a[(Math.floor(Math.random () * a.length ))]);
+    embed.setAuthor(`name`);
+    if(message.content.startsWith(prefix + "ruletka ")) {
+        message.reply(embed)
+    }
+    else if(message.content === prefix + "ruletka") {
+    const embedd = new Discord.RichEmbed();
+    embedd.setDescription(`Напишите вопрос`);
+    embedd.setColor(0xFFFFFF);    
+    embedd.setTimestamp();
+        message.reply(embedd)
+    }
+    
+});
+client.on(`message`, (message) => {
+    if(message.channel.id ===`414350089226551319`){
+    
+    message.react(`425616013149601792`);
+    message.react(`425616053641543680`);
+       
+    }
+});
+ 
